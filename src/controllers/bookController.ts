619e6a05c29dcf9ab560ad7d732fc89c4603767a
byEduarda <albuquerque.zxl@gmail.com>
2025-07-17 19:50:12 -0300

    Create bookController.ts

---
+++ src/controllers/bookController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import { Book } from '../models/Books';
+
+export async function createBook(req: Request, res: Response) {
+  try {
+    const book = new Book(req.body);
+    await book.save();
+    res.status(201).json(book);
+  } catch (err) {
+    res.status(400).json({ error: 'Erro ao salvar livro' });
+  }
+}
