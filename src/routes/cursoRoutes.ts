51f560bf8aa1838eb647a7a3569fe30dc84fb6d1
byEduarda <albuquerque.zxl@gmail.com>
2025-07-18 01:13:47 -0300

    Update cursoRoutes.ts

--- src/routes/cursoRoutes.ts
+++ src/routes/cursoRoutes.ts
@@ -3,7 +3,7 @@ import { criarCurso, listarCursos } from '../controllers/cursoController';
 
 const router = Router();
 
-router.post('/cursos', criarCurso);
-router.get('/cursos', listarCursos);
+router.post('/', criarCurso);
+router.get('/', listarCursos);
 
 export default router;
