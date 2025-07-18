3e398882ff49109446eacc2aa42603503cf8f11a
byEduarda <albuquerque.zxl@gmail.com>
2025-07-18 00:11:59 -0300

    Update mongoConnect.ts

--- src/infra/database/mongoConnect.ts
+++ src/infra/database/mongoConnect.ts
@@ -1,16 +1,14 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/escola';
 
-export async function connectMongoDB(): Promise<void> {
+export async function connectToMongo() {
   try {
-    await mongoose.connect(MONGO_URL);
-    console.log('✅ MongoDB conectado com sucesso');
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log('🟢 Conectado ao MongoDB Atlas com sucesso');
   } catch (error) {
-    console.error('Erro ao conectar no MongoDB:', error);
+    console.error('🔴 Erro ao conectar no MongoDB:', error);
     process.exit(1);
   }
 }
