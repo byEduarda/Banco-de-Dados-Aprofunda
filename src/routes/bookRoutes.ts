dc408230eaf55eaee250d315d77488cbfc0cbf6c
byEduarda <albuquerque.zxl@gmail.com>
2025-07-17 23:58:00 -0300

    Delete bookRoutes.ts

--- src/routes/bookRoutes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { createBook } from '../controllers/bookController';
-
-const router = Router();
-router.post('/books', createBook);
-export default router;
